838450b8a1e09710134d85d6784c7a357fc45cdc
alexc <ErlanGithub@gmail.com>
2024-03-25 09:27:55 -0400

    azure function authenticacion

---
+++ coling/ColingRepositorioAutenticacion/Implementacion/TokenData.cs
@@ -0,0 +1,15 @@
+﻿using ColingRepositorioAutenticacion.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColingRepositorioAutenticacion.Implementacion
+{
+    public class TokenData : ITokenData
+    {
+        public DateTime Expira { get; set; }
+        public string Token { get; set; }
+    }
+}
